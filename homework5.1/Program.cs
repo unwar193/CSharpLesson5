863cf9af26b717dc764cbcb245a7d4a9d9c011bc
unwar193 <varan193@mail.ru>
2022-10-05 10:20:41 +0300

    homework5

---
+++ homework5.1/Program.cs
@@ -0,0 +1,50 @@
+﻿/* Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2 */
+
+Console.Clear();
+
+Console.Write("Введите длину массива: ");
+int arrLength = int.Parse(Console.ReadLine()!);
+int[] array = new int[arrLength];
+
+FillArray(array);
+PrintArray(array);
+Console.Write($"Количество четных чисел в массиве: {EvenCount(array)}");
+
+void FillArray(int[] arr)
+{
+    for (int i = 0; i < arrLength; i++)
+    {
+        Random rand = new Random();
+        arr[i] = rand.Next(100, 1000);
+    }
+}
+
+int EvenCount(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            count++;
+        };       
+    }
+    return count;
+}
+
+
+void PrintArray(int[] arr)
+{
+    int count = arr.Length;
+    Console.Write("[");
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write(arr[i]);
+        if (i == count - 1)
+            Console.WriteLine("]");
+        else
+            Console.Write(", ");
+    }
+}
